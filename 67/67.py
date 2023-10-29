ed6cd4863813091ee0479f549a190391a55583b2
Le T. Anh <imr3ng4l@gmail.com>
2023-10-29 19:25:23 +0700

    add another solution (found online) that needs only linear space for problem 67

--- 67/67.py
+++ 67/67.py
@@ -12,3 +12,11 @@ for row, vals in enumerate(t[:0:-1], start=-len(t) + 1):
         max_val = max(t[row][idx], t[row][idx+1])
         t[row-1][idx] += max_val
 print(t[0][0])
+
+with open('67/0067_triangle.txt') as f:
+    prev = list()
+    for ln in f:
+        prev = [0] + prev + [0]
+        curr = map(int, ln.split())
+        prev = [max(a, b) + c for a, b, c in zip(prev[1:], prev[:-1], curr)]
+print(max(prev))
